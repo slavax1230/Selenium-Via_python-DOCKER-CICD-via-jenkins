9813272d5064bd3d5af888e07f3cae5dc2e57dd1
slavax1230 <slavax25@gmail.com>
2023-09-04 03:19:51 -0700

    automation 1.0

---
+++ main.py
@@ -0,0 +1,22 @@
+from selenium import webdriver
+from selenium.webdriver.chrome.service import Service
+from selenium.webdriver.common.by import By
+from selenium.webdriver.common.keys import Keys
+from selenium.webdriver.chrome.options import Options
+import time
+
+options = Options()
+options.add_experimental_option("detach", True)
+
+CHROME_DRIVER_PATH = r"C:\Users\NITO\OneDrive\TCB\devops\HM4.9\Selenium-Via_python-DOCKER-CICD-via-jenkins\driv\chromedriver.exe"
+SERVICE = Service(CHROME_DRIVER_PATH)
+DRIVER = webdriver.Chrome(service=SERVICE,options=options)
+
+class Getinfo:
+    def getTitle(self):
+        DRIVER.get("https://www.parks.org.il/newsflash/")
+        mivzak = DRIVER.find_elements(By.CLASS_NAME, "article_content")
+        for i in mivzak:
+            print(i.text)
+info = Getinfo()
+info.getTitle()
